158e3f048dc3008f244c4f0ae9d3e65a7f446607
semgoSE <semgoSE@gmail.com>
2022-11-02 02:39:12 +0300

    list okpd pagination

--- frontend/src/views/okpd/Okpd.js
+++ frontend/src/views/okpd/Okpd.js
@@ -1,39 +1,46 @@
 import {Button, Card, CardBody, CardHeader, CardText, CardTitle, Spinner} from "reactstrap";
-import DataTable from "react-data-table-component";
-import {columns} from "./list/columns";
-import {ChevronDown} from "react-feather";
-import ReactPaginate from "react-paginate";
 import {useEffect, useState} from "react";
 import {request} from "../../utility/request";
-import CardActions from "../../@core/components/card-actions";
 
 const Okpd = () => {
-    const [nextPage, setNextPage] = useState('');
-    const [okpdList, setOkpdList] = useState(null);
+    const [maxResults, setMaxResults] = useState(0)
+    const [nextPage, setNextPage] = useState('')
+    const [okpdList, setOkpdList] = useState(null)
+
+    const loadMore = async () => {
+        console.log(nextPage)
+        const data = await request('get', nextPage.split('.ru/')[1])
+        setNextPage(data.next)
+        setOkpdList(prev => [...prev, ...data.results])
+    }
 
     useEffect(() => {
         request('get', 'okpd/').then(data => {
             setNextPage(data.next)
             setOkpdList(data.results)
+            setMaxResults(data.count)
         })
     }, [])
 
     return (
         <Card>
-            <CardHeader>
-                <CardTitle>List okpd</CardTitle>
-            </CardHeader>
             <CardBody>
-                {okpdList ? <div className='react-List'>
+                {okpdList ? <div className='react-List block'>
                         {okpdList.map(e => <Card key={e.code}>
                             <CardTitle>{e.code} {e.name}</CardTitle>
-                            <Button.Ripple color='flat-danger'>Danger</Button.Ripple>
+                            <CardBody>
+                                <Button.Ripple color='flat-primary'>Редактирвать</Button.Ripple>
+                                <Button.Ripple color='flat-primary' style={{ marginRight: 12 }}>Удалить</Button.Ripple>
+                            </CardBody>
                         </Card>)}
                     </div> : <div>
                         <Spinner/>
                     </div>
                 }
             </CardBody>
+            {maxResults > okpdList?.length && <div style={{padding: 12, width: '100%'}}>
+                <Button onClick={loadMore} style={{width: '100%'}} color='flat-primary'>загрузить еще</Button>
+            </div>}
         </Card>
     )
 }
