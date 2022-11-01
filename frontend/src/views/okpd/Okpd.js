88db7fa79226240318aaa140636e9aaf52095de3
semgoSE <semgoSE@gmail.com>
2022-11-02 00:21:50 +0300

    list okpd

--- frontend/src/views/okpd/Okpd.js
+++ frontend/src/views/okpd/Okpd.js
@@ -1,68 +1,38 @@
-import {Card, CardBody, CardHeader, CardText, CardTitle} from "reactstrap";
+import {Button, Card, CardBody, CardHeader, CardText, CardTitle, Spinner} from "reactstrap";
 import DataTable from "react-data-table-component";
 import {columns} from "./list/columns";
 import {ChevronDown} from "react-feather";
 import ReactPaginate from "react-paginate";
-
-const CustomPagination = () => {
-    const count = 10;
-
-    return (
-        <ReactPaginate
-            previousLabel={''}
-            nextLabel={''}
-            pageCount={count || 1}
-            activeClassName='active'
-            forcePage={0 !== 0 ? 0 - 1 : 0}
-            // onPageChange={page => handlePagination(page)}
-            pageClassName={'page-item'}
-            nextLinkClassName={'page-link'}
-            nextClassName={'page-item next'}
-            previousClassName={'page-item prev'}
-            previousLinkClassName={'page-link'}
-            pageLinkClassName={'page-link'}
-            containerClassName={'pagination react-paginate justify-content-end my-2 pe-1'}
-        />
-    )
-}
+import {useEffect, useState} from "react";
+import {request} from "../../utility/request";
+import CardActions from "../../@core/components/card-actions";
 
 const Okpd = () => {
+    const [nextPage, setNextPage] = useState('');
+    const [okpdList, setOkpdList] = useState(null);
+
+    useEffect(() => {
+        request('get', 'okpd/').then(data => {
+            setNextPage(data.next)
+            setOkpdList(data.results)
+        })
+    }, [])
+
     return (
         <Card>
             <CardHeader>
                 <CardTitle>List okpd</CardTitle>
             </CardHeader>
             <CardBody>
-                <div>
-                    <Card className='overflow-hidden'>
-                        <div className='react-dataTable'>
-                            <DataTable
-                                noHeader
-                                subHeader
-                                sortServer
-                                pagination
-                                responsive
-                                paginationServer
-                                // columns={columns}
-                                // onSort={handleSort}
-                                sortIcon={<ChevronDown />}
-                                className='react-dataTable'
-                                paginationComponent={CustomPagination}
-                                // data={dataToRender()}
-                                // subHeaderComponent={
-                                //     <CustomHeader
-                                //         store={store}
-                                //         searchTerm={searchTerm}
-                                //         rowsPerPage={rowsPerPage}
-                                //         handleFilter={handleFilter}
-                                //         handlePerPage={handlePerPage}
-                                //         toggleSidebar={toggleSidebar}
-                                //     />
-                                // }
-                            />
-                        </div>
-                    </Card>
-                </div>
+                {okpdList ? <div className='react-List'>
+                        {okpdList.map(e => <Card key={e.code}>
+                            <CardTitle>{e.code} {e.name}</CardTitle>
+                            <Button.Ripple color='flat-danger'>Danger</Button.Ripple>
+                        </Card>)}
+                    </div> : <div>
+                        <Spinner/>
+                    </div>
+                }
             </CardBody>
         </Card>
     )
