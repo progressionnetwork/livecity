8f91e6d4b2e30026e1704ff203516ccc5e1f0350
semgoSE <semgoSE@gmail.com>
2022-11-03 02:26:07 +0300

    test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,20 @@
-import React, { Suspense } from "react"
+import React, {Suspense} from "react"
 
 // ** Router Import
 import Router from "./router/Router"
 import {AuthProvider} from "./utility/context/AuthContext";
 
 const App = () => {
-  return (
-    <Suspense fallback={null}>
-        <AuthProvider>
-            <Router />
-        </AuthProvider>
-    </Suspense>
-  )
+
+    console.log(process.env)
+
+    return (
+        <Suspense fallback={null}>
+            <AuthProvider>
+                <Router/>
+            </AuthProvider>
+        </Suspense>
+    )
 }
 
 export default App
