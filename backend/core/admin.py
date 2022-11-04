99cc03fd6de2c3debeff4608a72d58f9796098dc
Ruslan Khairullin <ruslan.gzn@gmail.com>
2022-11-04 22:35:17 +0500

    update admin

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from core.models import KPGZ, OKEI, OKPD, OKPD2, FileUpdate, SPGZ, SN, SNSection, SNSubsection, SNRow, SNSubRow
+from core.models import KPGZ, OKEI, OKPD, OKPD2, FileUpdate, SPGZ, SN, SNSection, SNSubsection, SNRow, SNSubRow, TZ, TZRow
 
 admin.site.register(KPGZ)
 admin.site.register(OKEI)
@@ -11,4 +11,5 @@ admin.site.register(SN)
 admin.site.register(SNSection)
 admin.site.register(SNSubsection)
 admin.site.register(SNRow)
-admin.site.register(SNSubRow)
+admin.site.register(TZ)
+admin.site.register(TZRow)
