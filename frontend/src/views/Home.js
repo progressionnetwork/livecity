7b3507e3abac9219bf748c62aa070e3f48b67bb0
semgoSE <semgoSE@gmail.com>
2022-11-06 11:32:50 +0300

    list smeta

--- frontend/src/views/Home.js
+++ frontend/src/views/Home.js
@@ -4,21 +4,29 @@ import {
   CardBody,
   CardTitle,
   CardText,
-  CardLink, Button
+  CardLink, Button, Input, Label, CardFooter, Spinner
 } from "reactstrap"
 
 import { useSelector } from "react-redux"
-import { useEffect } from "react"
+import {useEffect, useState} from "react"
 import {useNavigate} from "react-router-dom"
+
+import {request} from "../utility/request";
 
 const Home = () => {
   const nav = useNavigate()
   const user = useSelector(state => state.user)
+  const [listSmeta, setListSmeta] = useState(null);
 
   useEffect(() => {
     if (!user.data) {
       nav('/login')
     }
+
+    request('get', 'smeta/').then(data => {
+      setListSmeta(data)
+    })
+
   }, [])
 
   return (
@@ -40,9 +48,30 @@ const Home = () => {
             <Button color="primary" onClick={() => nav('/upload')}>Загрузить новую</Button>
         </CardHeader>
         <CardBody>
-
+          <Label>Поиск</Label>
+          <Input />
         </CardBody>
       </Card>
+
+      {!listSmeta && <Spinner />}
+
+      {listSmeta && listSmeta.map((e) => (
+          <Card>
+            <CardHeader>
+              <CardTitle>
+                {e.name}
+              </CardTitle>
+            </CardHeader>
+            <CardBody>
+              {e.address}
+            </CardBody>
+            <CardFooter>
+              <Button color="primary">Подробнее</Button>
+              <Button color="danger" style={{ marginLeft: 8 }}>Удалить</Button>
+            </CardFooter>
+          </Card>
+      ))}
+
     </div>
   )
 }
