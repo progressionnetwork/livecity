3374eb489c6ba99e70d1a8d0ae551ee3452d19d9
semgoSE <semgoSE@gmail.com>
2022-11-09 02:12:48 +0300

    add edit smeta

--- frontend/src/views/Smeta.js
+++ frontend/src/views/Smeta.js
@@ -49,7 +49,7 @@ import {numberWithSpaces} from "../utility/Utils";
 //     fontFamily: 'var(--bs-body-font-family)'
 // })
 
-const StyledTableCell = styled(TableCell)(({ theme }) => ({
+const StyledTableCell = styled(TableCell)(({theme}) => ({
     color: 'var(--bs-body-color)',
     fontSize: '1rem',
     fontFamily: 'var(--bs-body-font-family)'
@@ -132,29 +132,59 @@ const Subsection = (props) => {
                             <Table size="small">
                                 <TableHead>
                                     <TableRow>
-                                        <StyledTableCell sx={{ fontSize: '0.8rem', width: 70 }}>№ п/п</StyledTableCell>
-                                        <StyledTableCell sx={{ fontSize: '0.8rem', width: 120 }}>Шифр</StyledTableCell>
-                                        <StyledTableCell sx={{ fontSize: '0.8rem' }}>Название</StyledTableCell>
-                                        <StyledTableCell sx={{ fontSize: '0.8rem', width: 100 }}>Кол-во</StyledTableCell>
-                                        <StyledTableCell sx={{ fontSize: '0.8rem', width: 100 }}>Ед. изм.</StyledTableCell>
-                                        <StyledTableCell sx={{ fontSize: '0.8rem', width: 150 }}>Сумма</StyledTableCell>
-                                        <StyledTableCell sx={{ fontSize: '0.8rem', width: 150 }}>Является ключевой</StyledTableCell>
+                                        <StyledTableCell sx={{fontSize: '0.8rem', width: 70}}>№ п/п</StyledTableCell>
+                                        <StyledTableCell sx={{fontSize: '0.8rem', width: 120}}>Шифр</StyledTableCell>
+                                        <StyledTableCell sx={{fontSize: '0.8rem'}}>Название</StyledTableCell>
+                                        <StyledTableCell sx={{fontSize: '0.8rem', width: 100}}>Кол-во</StyledTableCell>
+                                        <StyledTableCell sx={{fontSize: '0.8rem', width: 100}}>Ед.
+                                            изм.</StyledTableCell>
+                                        <StyledTableCell sx={{fontSize: '0.8rem', width: 150}}>Сумма</StyledTableCell>
+                                        <StyledTableCell sx={{fontSize: '0.8rem', width: 150}}>Является
+                                            ключевой</StyledTableCell>
                                     </TableRow>
                                 </TableHead>
                                 <TableBody>
                                     {subsection.rows.map((row, i) => (
                                         <>
-                                            <TableRow sx={{ cursor: 'pointer' }} key={row.id} onClick={() => {
+                                            <TableRow sx={{cursor: 'pointer'}} key={row.id} onClick={() => {
                                                 if (openStats.includes(row.id)) setOpenStats(openStats.filter(e => e !== row.id))
                                                 else setOpenStats([...openStats, row.id])
                                             }}>
-                                                <StyledTableCell sx={{ fontSize: '0.8rem', color: 'white', backgroundColor: row.color }}>{row.num}</StyledTableCell>
-                                                <StyledTableCell sx={{ fontSize: '0.8rem', color: 'white', backgroundColor: row.color }}>{row.code}</StyledTableCell>
-                                                <StyledTableCell sx={{ fontSize: '0.8rem', color: 'white', backgroundColor: row.color }}>{row.name}</StyledTableCell>
-                                                <StyledTableCell sx={{ fontSize: '0.8rem', color: 'white', backgroundColor: row.color }}>{row.count}</StyledTableCell>
-                                                <StyledTableCell sx={{ fontSize: '0.8rem', color: 'white', backgroundColor: row.color }}>{row?.ei?.short_name}</StyledTableCell>
-                                                <StyledTableCell sx={{ fontSize: '0.8rem', color: 'white', backgroundColor: row.color }}>{numberWithSpaces(row.sum)}</StyledTableCell>
-                                                <StyledTableCell sx={{ fontSize: '0.8rem', color: 'white', backgroundColor: row.color }}><Switch onClick={(e) => {
+                                                <StyledTableCell sx={{
+                                                    fontSize: '0.8rem',
+                                                    color: 'white',
+                                                    backgroundColor: row.color
+                                                }}>{row.num}</StyledTableCell>
+                                                <StyledTableCell sx={{
+                                                    fontSize: '0.8rem',
+                                                    color: 'white',
+                                                    backgroundColor: row.color
+                                                }}>{row.code}</StyledTableCell>
+                                                <StyledTableCell sx={{
+                                                    fontSize: '0.8rem',
+                                                    color: 'white',
+                                                    backgroundColor: row.color
+                                                }}>{row.name}</StyledTableCell>
+                                                <StyledTableCell sx={{
+                                                    fontSize: '0.8rem',
+                                                    color: 'white',
+                                                    backgroundColor: row.color
+                                                }}>{row.count}</StyledTableCell>
+                                                <StyledTableCell sx={{
+                                                    fontSize: '0.8rem',
+                                                    color: 'white',
+                                                    backgroundColor: row.color
+                                                }}>{row?.ei?.short_name}</StyledTableCell>
+                                                <StyledTableCell sx={{
+                                                    fontSize: '0.8rem',
+                                                    color: 'white',
+                                                    backgroundColor: row.color
+                                                }}>{numberWithSpaces(row.sum)}</StyledTableCell>
+                                                <StyledTableCell sx={{
+                                                    fontSize: '0.8rem',
+                                                    color: 'white',
+                                                    backgroundColor: row.color
+                                                }}><Switch onClick={(e) => {
                                                     setSubsection(prev => {
                                                         request('patch', `smeta_row/${row.id}/`, {
                                                             is_key: !row.is_key
@@ -170,29 +200,45 @@ const Subsection = (props) => {
                                                 }} checked={row.is_key}/></StyledTableCell>
                                             </TableRow>
 
-                                            <TableRow sx={{ display: openStats.includes(row.id) ? undefined : 'none' }}>
-                                                <StyledTableCell sx={{ fontSize: '0.8rem', fontWeight: 800 }} colSpan={2}>СПГЗ по Fasttext</StyledTableCell>
-                                                <StyledTableCell sx={{ fontSize: '0.8rem' }}>{row.stats[0].fasttext_spgz}</StyledTableCell>
-                                                <StyledTableCell sx={{ fontSize: '0.8rem', fontWeight: 800 }} colSpan={2}>Вероятность по Fasttext</StyledTableCell>
-                                                <StyledTableCell sx={{ fontSize: '0.8rem' }}>{row.stats[0].fasstext_percent}</StyledTableCell>
+                                            <TableRow sx={{display: openStats.includes(row.id) ? undefined : 'none'}}>
+                                                <StyledTableCell sx={{fontSize: '0.8rem', fontWeight: 800}} colSpan={2}>СПГЗ
+                                                    по Fasttext</StyledTableCell>
+                                                <StyledTableCell
+                                                    sx={{fontSize: '0.8rem'}}>{row.stats[0].fasttext_spgz}</StyledTableCell>
+                                                <StyledTableCell sx={{fontSize: '0.8rem', fontWeight: 800}} colSpan={2}>Вероятность
+                                                    по Fasttext</StyledTableCell>
+                                                <StyledTableCell
+                                                    sx={{fontSize: '0.8rem'}}>{row.stats[0].fasstext_percent}</StyledTableCell>
                                             </TableRow>
-                                            <TableRow sx={{ display: openStats.includes(row.id) ? undefined : 'none' }}>
-                                                <StyledTableCell sx={{ fontSize: '0.8rem', fontWeight: 800 }} colSpan={2}>СПГЗ по ключевым фразам</StyledTableCell>
-                                                <StyledTableCell sx={{ fontSize: '0.8rem' }}>{row.stats[0].key_phrases_spgz}</StyledTableCell>
-                                                <StyledTableCell sx={{ fontSize: '0.8rem', fontWeight: 800 }} colSpan={2}>Вероятность по ключевым фразам</StyledTableCell>
-                                                <StyledTableCell sx={{ fontSize: '0.8rem' }}>{row.stats[0].key_phrases_percent}</StyledTableCell>
+                                            <TableRow sx={{display: openStats.includes(row.id) ? undefined : 'none'}}>
+                                                <StyledTableCell sx={{fontSize: '0.8rem', fontWeight: 800}} colSpan={2}>СПГЗ
+                                                    по ключевым фразам</StyledTableCell>
+                                                <StyledTableCell
+                                                    sx={{fontSize: '0.8rem'}}>{row.stats[0].key_phrases_spgz}</StyledTableCell>
+                                                <StyledTableCell sx={{fontSize: '0.8rem', fontWeight: 800}} colSpan={2}>Вероятность
+                                                    по ключевым фразам</StyledTableCell>
+                                                <StyledTableCell
+                                                    sx={{fontSize: '0.8rem'}}>{row.stats[0].key_phrases_percent}</StyledTableCell>
                                             </TableRow>
-                                            <TableRow sx={{ display: openStats.includes(row.id) ? undefined : 'none' }}>
-                                                <StyledTableCell sx={{ fontSize: '0.8rem', fontWeight: 800 }} colSpan={2}>Ключевые слова с вероятностями</StyledTableCell>
-                                                <StyledTableCell sx={{ fontSize: '0.8rem' }}>{row.stats[0].stat_words.map(e => `${e.name}(${e.percent})`).join(', ')}</StyledTableCell>
-                                                <StyledTableCell sx={{ fontSize: '0.8rem', fontWeight: 800 }} colSpan={2}>Рассотяние Левенштейна</StyledTableCell>
-                                                <StyledTableCell sx={{ fontSize: '0.8rem' }}>{row.stats[0].levenst_ratio}</StyledTableCell>
+                                            <TableRow sx={{display: openStats.includes(row.id) ? undefined : 'none'}}>
+                                                <StyledTableCell sx={{fontSize: '0.8rem', fontWeight: 800}} colSpan={2}>Ключевые
+                                                    слова с вероятностями</StyledTableCell>
+                                                <StyledTableCell
+                                                    sx={{fontSize: '0.8rem'}}>{row.stats[0].stat_words.map(e => `${e.name}(${e.percent})`).join(', ')}</StyledTableCell>
+                                                <StyledTableCell sx={{fontSize: '0.8rem', fontWeight: 800}} colSpan={2}>Рассотяние
+                                                    Левенштейна</StyledTableCell>
+                                                <StyledTableCell
+                                                    sx={{fontSize: '0.8rem'}}>{row.stats[0].levenst_ratio}</StyledTableCell>
                                             </TableRow>
-                                            <TableRow sx={{ display: openStats.includes(row.id) ? undefined : 'none' }}>
-                                                <StyledTableCell sx={{ fontSize: '0.8rem', fontWeight: 800 }} colSpan={2}>Является ключевой позицией</StyledTableCell>
-                                                <StyledTableCell sx={{ fontSize: '0.8rem' }}>{row.stats[0].is_key ? 'Да' : 'Нет'}</StyledTableCell>
-                                                <StyledTableCell sx={{ fontSize: '0.8rem', fontWeight: 800 }} colSpan={2}>Вероятность ключевой позиции</StyledTableCell>
-                                                <StyledTableCell sx={{ fontSize: '0.8rem' }}>{row.stats[0].key_percent}</StyledTableCell>
+                                            <TableRow sx={{display: openStats.includes(row.id) ? undefined : 'none'}}>
+                                                <StyledTableCell sx={{fontSize: '0.8rem', fontWeight: 800}} colSpan={2}>Является
+                                                    ключевой позицией</StyledTableCell>
+                                                <StyledTableCell
+                                                    sx={{fontSize: '0.8rem'}}>{row.stats[0].is_key ? 'Да' : 'Нет'}</StyledTableCell>
+                                                <StyledTableCell sx={{fontSize: '0.8rem', fontWeight: 800}} colSpan={2}>Вероятность
+                                                    ключевой позиции</StyledTableCell>
+                                                <StyledTableCell
+                                                    sx={{fontSize: '0.8rem'}}>{row.stats[0].key_percent}</StyledTableCell>
                                             </TableRow>
                                         </>
 
@@ -217,6 +263,11 @@ const Smeta = () => {
     const [isModalEdit, setIsModalEdit] = useState(false)
 
     const updateSmeta = async () => {
+        request('patch', `smeta/${smeta.id}/`, editItem).then()
+        setSmeta(prev => ({
+            ...prev,
+            ...editItem
+        }))
         setIsModalEdit(false)
     }
 
@@ -280,8 +331,19 @@ const Smeta = () => {
                             </div>
                         </Stack>
                         <Stack spacing={1} direction="row" mt={1}>
-                            <Button color="primary">Редактировать</Button>
-                            <Button color="success" outline style={{ marginLeft: 8 }} onClick={() => {
+                            <Button color="primary" onClick={() => {
+                                setEditItem({
+                                    name: smeta.name,
+                                    address: smeta.address,
+                                    sum: smeta.sum,
+                                    tax: smeta.tax,
+                                    sum_with_tax: smeta.sum_with_tax,
+                                    coef_date: smeta.coef_date
+                                })
+                                setIsModalEdit(true)
+                            }
+                            }>Редактировать</Button>
+                            <Button color="success" outline style={{marginLeft: 8}} onClick={() => {
 
                                 fetch(`https://api.livecity.goodgenius.ru/smeta/${smeta.id}/excel/`, {
                                     method: 'GET',
@@ -337,11 +399,57 @@ const Smeta = () => {
                         <Label className='form-label' for='name'>
                             Название:
                         </Label>
-                        <Input type='name' id='name' value={editItem?.value} onChange={(e => setEditItem(prev => ({
+                        <Input type='name' id='name' value={editItem?.name} onChange={(e => setEditItem(prev => ({
                             ...prev,
-                            value: e.target.value
+                            name: e.target.value
                         })))} placeholder='Название'/>
                     </div>
+                    <div>
+                        <Label className='form-label' for='name'>
+                            Адрес:
+                        </Label>
+                        <Input type='name' id='name' value={editItem?.address} onChange={(e => setEditItem(prev => ({
+                            ...prev,
+                            address: e.target.value
+                        })))} placeholder='Адрес'/>
+                    </div>
+                    <div>
+                        <Label className='form-label' for='name'>
+                            Сумма без НДС:
+                        </Label>
+                        <Input type='name' id='name' value={editItem?.sum} onChange={(e => setEditItem(prev => ({
+                            ...prev,
+                            sum: e.target.value
+                        })))} placeholder='  Сумма без НДС'/>
+                        <div>
+                            <Label className='form-label' for='name'>
+                                НДС:
+                            </Label>
+                            <Input type='name' id='name' value={editItem?.tax} onChange={(e => setEditItem(prev => ({
+                                ...prev,
+                                tax: e.target.value
+                            })))} placeholder='  Сумма без НДС'/>
+                        </div>
+                        <Label className='form-label' for='name'>
+                            Сумма с НДС:
+                        </Label>
+                        <Input type='name' id='name' value={editItem?.sum_with_tax}
+                               onChange={(e => setEditItem(prev => ({
+                                   ...prev,
+                                   sum_with_tax: e.target.value
+                               })))} placeholder='Сумма с НДС'/>
+
+                        <Label className='form-label' for='name'>
+                            Дата:
+                        </Label>
+                        <Input type='date' id='name' value={editItem?.coef_date} onChange={(e => setEditItem(prev => ({
+                            ...prev,
+                            coef_date: e.target.value
+                        })))} placeholder='Дата'/>
+
+                    </div>
+
+
                 </ModalBody>
                 <ModalFooter>
                     <Button color='primary' onClick={updateSmeta}>
